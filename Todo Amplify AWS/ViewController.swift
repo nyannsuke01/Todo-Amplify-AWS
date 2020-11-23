b754350db377043650c56080c0053dbe16b8a5c5
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-11-23 23:14:56 +0900

    Initial Commit

---
+++ Todo Amplify AWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo Amplify AWS
+//
+//  Created by user on 2020/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
